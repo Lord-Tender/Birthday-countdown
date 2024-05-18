c98f710b9fc8f21b4b29583b698e984e6e74faa9
Emmanuel <emmanuelola961@gmail.com>
2024-05-18 12:46:18 +0100

    everything done!

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ const showCounter = () => {
 const cancelCounter = () => {
     document.querySelector('.counter').style.display = 'none'
 }
+
+const countDown = () => {
+    let countDownDate = new Date('2024-05-24')
+    let now = new Date()
+    let difference = countDownDate - now
+    let date = new Date(difference)
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    document.getElementById('day').innerHTML = day
+    document.getElementById('hour').innerHTML = hours
+    document.getElementById('minute').innerHTML = minutes
+    document.getElementById('second').innerHTML = seconds
+    if (difference == 0) {
+        document.getElementById('theDay').style.display = "block"
+        document.getElementById('theDay').innerHTML = "It's your day, HBD dear!!!"
+    }
+}
+
+setInterval(() => {
+    countDown()
+}, 1000);
+
+
+
+document.getElementById('whatsappButton').addEventListener('click', function() {
+    const phoneNumber = '+2347044959836'; 
+    const message = encodeURIComponent("Hello, Can you design a birthday count down website for me?");
+
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(whatsappURL, '_blank');
+});
