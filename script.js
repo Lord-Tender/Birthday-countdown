85d133c22894a03defe1861a37742b7df445ffbb
Emmanuel <emmanuelola961@gmail.com>
2024-05-18 11:44:03 +0100

    Counter interface added!

---
+++ script.js
@@ -0,0 +1,7 @@
+const showCounter = () => {
+    document.querySelector('.counter').style.display = 'flex'
+}
+
+const cancelCounter = () => {
+    document.querySelector('.counter').style.display = 'none'
+}
